5c1d6b59e3c3d55e3b3984a8f504c4a267cdf327
Sympart <115144506+Sympart@users.noreply.github.com>
2024-04-14 13:50:32 +0530

    Mac Changes

---
+++ java2.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll("#image-window img");
+    var index = 0;
+
+    function fadeInImage() {
+        images[index].style.opacity = "1"; // Fade in current image
+        index++; // Move to next image
+        if (index < images.length) {
+            setTimeout(fadeInImage, 1000); // Delay before fading in next image
+        }
+    }
+
+    fadeInImage(); // Start fading in images
+});
